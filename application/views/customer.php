b81c64c80c4f27826efb1bda4b10575e490a086b
fachrull <fachrulpaul@gmail.com>
2018-12-25 01:02:39 +0700

    update customer profile

--- application/views/customer.php
+++ application/views/customer.php
@@ -1,3 +1,30 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
-<h1>This is customer page.</h1>
+<div>
+  <div class="row">
+    <label for="uname">Name</label>
+    <input type="text" name="name" value="Fachrul" readonly>
+  </div>
+  <div class="row">
+    <label for="date">Born of date</label>
+    <input type="text" name="date" value="13 Juni 1995" readonly>
+  </div>
+  <div class="row">
+    <label for="gender">Gender</label>
+    <input type="text" name="gender" value="Laki-laki">
+  </div>
+  <div class="row">
+    <label for="email">Email</label>
+    <input type="email" name="email" value="fachrul@koliho.com" readonly>
+  </div>
+  <div class="row">
+    <label for="phone1">No Handphone</label>
+    <input type="number" name="phone1" value="085717778913" readonly>
+  </div>
+  <div class="row">
+    <a href="#" class="btn">Edit Profile</a>
+    <div class="right">
+      <a href="#" class="btn warning">Change Password</a>
+    </div>
+  </div>
+</div>
