facd71a95d65813f7cf2e0e831e75614aa518cb0
fachrull <fachrulpaul@gmail.com>
2018-12-29 23:53:52 +0700

    adding new feature 'super admin adding brands' view

---
+++ application/views/sa_brand.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<br>
+<a href="#" class="btn btn-oldblue"><i class="fa fa-plus"></i>Brand</a>
+<br>
+
+<h1>This Feature Will be updated soon.</h1>
+<table class="table">
+  <thead>
+
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
