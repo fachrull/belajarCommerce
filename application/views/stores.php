17d91ee0efc53f9c479238a89e585c31cde5b7d5
fachrull <fachrulpaul@gmail.com>
2018-12-29 08:05:48 +0700

    fix button detail stores view

--- application/views/stores.php
+++ application/views/stores.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
 <br><a href="<?=site_url('auth/regis');?>" class="btn btn-oldblue"><i class="fa fa-plus"></i>Store</a><br>
-
-<table class="bordered">
+<br>
+<table class="table">
   <thead>
     <tr>
       <th>Company Name</th>
@@ -25,7 +25,7 @@
         <td><?= $post['city'];?></td>
         <td><?= $post['province'];?></td>
         <td><?= $post['phone1'];?></td>
-        <td><a href="<?=site_url('home/adminStores/'.$post['id']);?>" class="btn">Detail</a></td>
+        <td><a href="<?=site_url('home/index/'.$post['id']);?>" type="submit" class="btn btn-oldblue text-white">Detail</a></td>
       </tr>
     <?php endforeach; ?>
   </tbody>
