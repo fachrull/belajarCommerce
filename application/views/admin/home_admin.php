093c4b70eee3d07bb4488a1eca5ed22b7afe8ad2
fachrull <fachrulpaul@gmail.com>
2019-01-04 20:57:18 +0700

    fix home view for super admin

--- application/views/admin/home_admin.php
+++ application/views/admin/home_admin.php
@@ -1,111 +1,39 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <h1>
       Home
-      <?php if($this->session->userdata('uType') == 1): ?>
-        <small>Super Admin</small>
-      <?php else: ?>
-        <small>Admin</small>
-      <?php endif; ?>
+        <small> - All Admin</small>
     </h1>
   </section>
-
-  <!-- Main content -->
-  <section class="content">
-    <!-- Small boxes (Stat box) -->
-    <div class="row">
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-          <div class="inner">
-            <h3>150</h3>
-
-            <p>New Orders</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    <!-- Main content -->
+    <section class="content">
+      <!-- Main row -->
+      <div class="container">
+        <div class="row">
+          <table class="table">
+            <thead>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Phone</th>
+              <th>Detail</th>
+            </thead>
+            <tbody>
+              <?php foreach($posts as $post): ?>
+                <tr>
+                  <td><?= $post['first_name'];?></td>
+                  <td><?= $post['last_name'];?></td>
+                  <td><?= $post['phone'];?></td>
+                  <td><a href="#" class="btn btn-default">Detail</a></td>
+                </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
       </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+      <!-- /.row (main row) -->
 
-            <p>Bounce Rate</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-stats-bars"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-          <div class="inner">
-            <h3>44</h3>
-
-            <p>User Registrations</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-red">
-          <div class="inner">
-            <h3>65</h3>
-
-            <p>Unique Visitors</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-pie-graph"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-    </div>
-    <!-- /.row -->
-    <!-- Main row -->
-    <div class="container">
-      <div class="row">
-        <h1>All Admin</h1><br>
-        <table class="table">
-          <thead>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Phone</th>
-            <th>Detail</th>
-          </thead>
-          <tbody>
-            <?php foreach($posts as $post): ?>
-              <tr>
-                <td><?= $post['first_name'];?></td>
-                <td><?= $post['last_name'];?></td>
-                <td><?= $post['phone'];?></td>
-                <td><a href="#" class="btn btn-default">Detail</a></td>
-              </tr>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
-
-      </div>
-    </div>
-    <!-- /.row (main row) -->
-
-  </section>
-  <!-- /.content -->
-</div>
+    </section>
+    <!-- /.content -->
+  </div>
