e8a1c36e5bfe031a7ba0ec2121cfe0591c98b4b1
fachrull <fachrulpaul@gmail.com>
2019-01-06 03:06:24 +0700

    adding custom css

--- application/views/include/admin/header.php
+++ application/views/include/admin/header.php
@@ -41,6 +41,7 @@
   <link rel="stylesheet" href="<?= base_url('asset/bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');?>">
   <!-- iCheck for checkboxes and radio inputs -->
   <link rel="stylesheet" href="<?= base_url('asset/plugins/iCheck/all.css');?>">
+  <link rel="stylesheet" href="<?= base_url('asset/css/custom.css')?>">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
