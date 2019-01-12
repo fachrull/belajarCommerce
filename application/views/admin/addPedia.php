c3982a09a79b55726cb6810350c4e65c57c9e97f
fachrull <fachrulpaul@gmail.com>
2019-01-12 19:03:09 +0700

    update adding agm pedia view

--- application/views/admin/addPedia.php
+++ application/views/admin/addPedia.php
@@ -1,6 +1,5 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="content-wrapper">
-<<<<<<< HEAD
   <section class="content-header">
     <h1>
       AGM
@@ -46,41 +45,4 @@
       </div>
     </div>
   </section>
-=======
-  <section class="content">
-      <div id="page-inner">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <p>Form Input</p>
-                            </div>
-                            <div class="panel-body">
-                                <?php if($this->session->flashdata('error')): ?>
-                                <div class="alert alert-danger">
-                  <strong>Alert!</strong> <?php echo $this->session->flashdata('error'); ?>
-                </div>
-                                <?php endif; ?>
-                                <form action="<?php echo site_url('home/pediaInputProcess'); ?>" method="post" enctype="multipart/form-data">
-                                    <div class="form-group"> 
-                                        <label for="">Title</label>
-                                        <input type="text" name="title" class="form-control" placeholder="Title" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="">Content</label>
-                                        <textarea name="content" id="isi" cols="30" rows="10" class="form-control" placeholder="Content News">
-                                        </textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="">Photo</label>
-                                        <input type="file" name="photo" required>
-                                    </div>
-                                    <input type="submit" class="btn btn-primary" value="Input">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-    </section>
->>>>>>> 2d924f0472adb59308389fdc4217121d2bd95949
 </div>
