0336d605327fe6d3612296eb8939cc5b5a3c5d93
fachrull <fachrulpaul@gmail.com>
2019-01-12 19:15:23 +0700

    update product detail

--- application/views/admin/prodDetail.php
+++ application/views/admin/prodDetail.php
@@ -7,7 +7,6 @@
     </h1>
   </section>
   <section class="content">
-<<<<<<< HEAD
     <img src="<?= base_url('asset/upload/'.$product['pict']);?>" alt="<?= $product['name'];?>" style="max-height: 388px; max-width: 405px;">
     <p>Brand: <b><?= $brand['name'];?></b></p>
     <p>Category: <b><?= $cat['name'];?></b></p>
@@ -18,8 +17,5 @@
     <p>Foundation Type: <?= $product['foundation'];?></p>
     <p>Size : <?= $product['size'];?></p>
     <textarea rows="8" cols="80" disabled><?= $product['description'];?></textarea>
-=======
-    <h1>This feature will be updated soon</h1>
->>>>>>> 2d924f0472adb59308389fdc4217121d2bd95949
   </section>
 </div>
