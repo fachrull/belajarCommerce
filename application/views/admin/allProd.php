3e1e863f4fe4a46acc907b2421d38d323c31e70c
fachrull <fachrulpaul@gmail.com>
2019-01-12 19:14:17 +0700

    update view for all product

--- application/views/admin/allProd.php
+++ application/views/admin/allProd.php
@@ -10,11 +10,7 @@
       <div class="col-xs">
         <div class="box">
           <div class="box-header pb-0">
-<<<<<<< HEAD
           <a href="<?= site_url('admin/addProd');?>" class="btn btn-oldblue h-30"><i class="fa fa-plus"></i> Product</a>
-=======
-          <a href="<?= site_url('home/addProd');?>" class="btn btn-oldblue h-30"><i class="fa fa-plus"></i> Product</a>
->>>>>>> 2d924f0472adb59308389fdc4217121d2bd95949
           </div>
           <div class="box-body">
           <table id="dataTable" class="table table-bordered table-striped">
@@ -29,23 +25,15 @@
       <tbody>
         <?php foreach ($products as $product): ?>
           <tr>
-            <td>
-<<<<<<< HEAD
+            <td
               <a href="<?= site_url('admin/delProd/'.$product['id'])?>" class="btn btn-oldblue">Delete</a>
-=======
-              <a href="<?= site_url('home/delProd/'.$product['id'])?>" class="btn btn-oldblue">Delete</a>
->>>>>>> 2d924f0472adb59308389fdc4217121d2bd95949
             </td>
             <td><?= $product['name'];?></td>
             <td><?= $product['price'];?></td>
             <td><?= $product['sub_price'];?></td>
             <td><?= $product['quantity'];?></td>
             <td>
-<<<<<<< HEAD
               <a href="<?= site_url('admin/allProd/'.$product['id']);?>" class="btn btn-oldblue">Detail</a>
-=======
-              <a href="<?= site_url('home/allProd/'.$product['id']);?>" class="btn btn-oldblue">Detail</a>
->>>>>>> 2d924f0472adb59308389fdc4217121d2bd95949
             </td>
           </tr>
         <?php endforeach; ?>
