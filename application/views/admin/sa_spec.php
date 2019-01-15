ed9bd1f2e793093e7f5fba3827239e41bd5d8317
fachrull <fachrulpaul@gmail.com>
2019-01-15 17:43:38 +0700

    view for list of spec

---
+++ application/views/admin/sa_spec.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="content-wrapper">
+  <section class="content-header">
+    <h1>List spec</h1>
+  </section>
+  <section class="content">
+    <div class="row">
+      <div class="box">
+        <div class="box-header pb-0">
+          <a href="<?= site_url('admin/addSpec');?>" class="btn btn-oldblue h-30">
+            <i class="fa fa-plus"></i>
+             Spec
+          </a>
+        </div>
+        <div class="box-body">
+          <table id="dataTable" class="table table-bordered table-striped">
+            <thead>
+              <th>No.</th>
+              <th>Name</th>
+              <!-- <th>Detail</th> -->
+              <th>Delete</th>
+            </thead>
+            <tbody>
+              <?php $no=1; ?>
+              <?php foreach ($specs as $spec):?>
+                <tr>
+                  <td><?= $no?></td>
+                  <td><?= $spec['name'];?></td>
+                  <!-- <td><a href="<?= site_url(''.$spec['id']);?>" class="btn btn-default"><i class="fa fa-edit"></i></a></td> -->
+                  <td><a href="<?= site_url('admin/deleteSpec/'.$spec['id']);?>" class="btn btn-danger" onclick="return confirm('Are you sure?')"><i class="fa fa-trash"></i></a></td>
+                </tr>
+                <?php $no++; ?>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
