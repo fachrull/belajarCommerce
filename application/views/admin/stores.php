cf97ec1ff0f6e660725e3b188feb30622c5216ee
fachrull <fachrulpaul@gmail.com>
2019-01-17 11:15:39 +0700

    create view for list of stores

--- application/views/admin/stores.php
+++ application/views/admin/stores.php
@@ -15,7 +15,6 @@
           <thead>
             <tr>
               <th>Company Name</th>
-              <th>Owner Name</th>
               <th>Address</th>
               <th>Sub District</th>
               <th>City</th>
@@ -28,13 +27,12 @@
             <?php foreach($posts as $post): ?>
               <tr>
                 <td><?= $post['company_name'];?></td>
-                <td><?= $post['owner_name'];?></td>
                 <td><?= $post['address'];?></td>
                 <td><?= $post['sub_district'];?></td>
                 <td><?= $post['city'];?></td>
                 <td><?= $post['province'];?></td>
                 <td><?= $post['phone1'];?></td>
-                <td><a href="<?=site_url('home/index/'.$post['id']);?>" type="submit" class="btn btn-oldblue text-white">Detail</a></td>
+                <td><a href="<?=site_url('admin/stores/'.$post['id']);?>" type="submit" class="btn btn-oldblue text-white">Detail</a></td>
               </tr>
             <?php endforeach; ?>
           </tbody>
