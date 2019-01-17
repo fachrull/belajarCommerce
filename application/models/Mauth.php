d6713526edeaefc35e2dbc1d26614f174470811b
fachrull <fachrulpaul@gmail.com>
2019-01-17 11:10:34 +0700

    share some functions from mhome

--- application/models/Mauth.php
+++ application/models/Mauth.php
@@ -18,6 +18,65 @@ class Mauth extends CI_Model{
   //   $this->db->insert('user_login',$data);
   // }
 
+  public function getDataIndex($id = FALSE){
+    if($this->session->userdata('uType') == 1){
+      if($id === FALSE){
+        $query = $this->db->get('tm_super_admin');
+        return $query->result_array();
+      }else{
+        $query = $this->db->get_where('tm_super_admin', array('id' => $id));
+        return $query->row_array();
+      }
+    } elseif ($this->session->userdata('uType') == 2) {
+      if($id === FALSE){
+        $query = $this->db->get('tm_store_owner');
+        return $query->result_array();
+      }else{
+        $query = $this->db->get_where('tm_store_owner', array('id' => $id));
+        return $query->row_array();
+      }
+    }
+  }
+
+  public function regisSO(){
+    $uType = 3;
+    $newOwner = 1;
+    $pass = 'store_owner_agm';
+    $created = $this->session->userdata('uId');
+    $dataUserLogin = array(
+      'username'      => $this->input->post('uname'),
+      'password'      => password_hash(($pass), PASSWORD_DEFAULT),
+      'email'         => $this->input->post('email'),
+      'user_type'     => $uType,
+      'newer'         => $newOwner,
+      'created'       => $created
+    );
+    $queryULoging = $this->db->insert('user_login', $dataUserLogin);
+
+    $userId = $this->getData(array('username' => $this->input->post('uname')),
+      array('userIdField' => 'user_id'), TRUE);
+    $id = $userId->user_id;
+
+    $dataStoreOwner = array(
+      'id_userLogin'    => $id,
+      'company_name'    => $this->input->post('company_name'),
+      'address'         => $this->input->post('add'),
+      'address2'        => $this->input->post('add2'),
+      'sub_district'    => $this->input->post('sub_district'),
+      'city'            => $this->input->post('city'),
+      'province'        => $this->input->post('province'),
+      'postcode'        => $this->input->post('pCode'),
+      'phone1'          => $this->input->post('phone1'),
+      'fax'             => $this->input->post('fax'),
+      'id_super_admin'  => $this->session->userdata('uId')
+    );
+    $queryStoreOwner = $this->db->insert('tm_store_owner', $dataStoreOwner);
+    return array(
+      'queryULoging'    => $queryULoging,
+      'queryStoreOwner' => $queryStoreOwner
+    );
+  }
+
   public function getData($condition=NULL, $selection=NULL, $singleRowResult = FALSE){
 
     // if we are selecting some condition
