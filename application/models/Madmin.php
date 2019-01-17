527492f1e87957adc2f578a625eb440ee4850f97
fachrull <fachrulpaul@gmail.com>
2019-01-17 11:10:14 +0700

    share some functions from mhome

--- application/models/Madmin.php
+++ application/models/Madmin.php
@@ -64,4 +64,20 @@ class Madmin extends CI_Model {
     );
     return $this->db->insert($table, $items);
   }
+
+  public function getDataIndex($id = FALSE){
+    if($id === FALSE){
+      $query = $this->db->get('tm_store_owner');
+      return $query->result_array();
+    }else{
+      $query = $this->db->get_where('tm_store_owner', array('id' => $id));
+      return $query->row_array();
+    }
+  }
+
+  public function dataPrime($id = NULL){
+    $this->db->select(array('emailField' => 'email'));
+    $query = $this->db->get_where('user_login', array('user_id' =>$id));
+    return $query->row_array();
+  }
 }
