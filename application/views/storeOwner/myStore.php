859cd3e8d3a808dab6c86d05984490061643967b
fachrull <fachrulpaul@gmail.com>
2019-01-17 11:11:34 +0700

    create new folder for all view for store owner

---
+++ application/views/storeOwner/myStore.php
@@ -0,0 +1,35 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="content-wrapper">
+  <section class="content-header">
+    Store Owner
+  </section>
+  <section class="content">
+    <div class="box">
+      <div class="box-header">
+        <h1>Product list</h1>
+      </div>
+      <div class="box-body">
+        <table id="dataTable" class="table table-bordered table-striped">
+          <thead>
+            <th>No.</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Sub Price</th>
+            <th>Quantity</th>
+          </thead>
+          <tbody>
+
+              <tr>
+                <td>1.</td>
+                <td>Imperal Heritage</td>
+                <td>Rp 1000000</td>
+                <td>Rp 1500000</td>
+                <td>-</td>
+              </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+</div>
