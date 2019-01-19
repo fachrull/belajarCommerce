5d4d4f6b60b694dae43a401d5ba4a151ce6bc831
fachrull <fachrulpaul@gmail.com>
2019-01-19 22:34:50 +0700

    create view detail product on store owner view

---
+++ application/views/storeOwner/detail_prodStore.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="content-wrapper">
+  <section class="content-header">
+    <h1>
+      <?= $product['name']?>
+      <small> product</small>
+    </h1>
+  </section>
+  <section class="content">
+    <img src="<?= base_url('asset/upload/'.$product['image']);?>" alt="<?= $product['name'];?>" style="max-height: 388px; max-width: 405px;">
+    <br><hr>
+    <div class="row">
+      <div class="col-sm-3">
+        <h3>Brand : <?= $brand['name'];?></h3>
+      </div>
+      <div class="col-sm-3">
+        <h3>Category: <?= $cat['name'];?></h3>
+      </div>
+      <div class="col-sm-12">
+        <h5>Price : Rp. <?= $product['price'];?></h5>
+      </div>
+      <div class="col-sm-12">
+        <h5>Sub Price : Rp <?=$product['sub_price'];?></h5>
+      </div>
+      <div class="col-sm-12">
+        <h5>Specification : <?=$spec['name'];?></h5>
+      </div>
+      <div class="col-sm-12">
+        <h5>Size : <?=$size['name'];?> (<?=$size['size'];?>)</h5>
+      </div>
+      <div class="col-sm-12">
+        <h5>Quantity : <?= ($Qnt['quantity'] == NULL? '-':$Qnt['quantity']);?></h5>
+      </div>
+      <div class="col-sm-12">
+        <textarea rows="8" cols="80" disabled><?= $product['description'];?></textarea>
+      </div>
+    </div><br>
+    <a href="<?= site_url('stores/storeProduct');?>" class="btn btn-default">Back</a>
+  </section>
+</div>
