06af7d6d6164b0a77bebfb426deddae23b330dde
Adrian Faisal <adrianfaisal@aol.com>
2019-04-08 10:06:23 +0700

    Changes

--- application/views/admin/addProd-2.php
+++ application/views/admin/addProd-2.php
@@ -112,9 +112,9 @@
 							</div>
 							<label class="input"><b>Upload product image</b>
 							<p class="help-block text-danger fs-12">Min. Size 2 MB and Resolution 700 x 670 pixels</p>
-								<input type="file" class="mt-5" name="productPict[]" />
-                                <input type="file" class="mt-5" name="productPict[]" />
-                                <input type="file" class="mt-5" name="productPict[]" />
+								<input type="file" class="mt-5" name="productPict" />
+<!--                                <input type="file" class="mt-5" name="productPict[]" />-->
+<!--                                <input type="file" class="mt-5" name="productPict[]" />-->
 							</label>
 							<div class="row mt-10">
 								<div class="col-md-6 text-left">
