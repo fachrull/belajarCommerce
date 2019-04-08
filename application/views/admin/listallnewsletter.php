7efa69d80263945dcdecd2389a6f752f09e6ee87
Adrian Faisal <adrianfaisal@aol.com>
2019-04-08 10:19:26 +0700

    Another changes

--- application/views/admin/listallnewsletter.php
+++ application/views/admin/listallnewsletter.php
@@ -2,7 +2,7 @@
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
-            Newsletter
+            Incoming Subscriber
         </h1>
     </section>
     <section class="content">
