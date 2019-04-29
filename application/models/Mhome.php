7a3f8a78fdd11e65e4903571ec70ab7e5393c611
Adrian Faisal <adrianfaisal@aol.com>
2019-04-29 19:26:59 +0700

    Fixed another product image

--- application/models/Mhome.php
+++ application/models/Mhome.php
@@ -163,11 +163,11 @@ class Mhome extends CI_Model{
 
   public function getProduct_MaxMinPrice($idProduct){
     $this->db->select('a.id, MAX(a.price) as max_price, MIN(a.price) as min_price, b.name, b.id, b.brand_id, b.cat_id,
-      b.description, b.image, d.stars');
+      b.description, d.image_1 as image, b.stars');
     $this->db->from('tr_product_size a');
     $this->db->join('tm_product b', 'b.id = a.prod_id', 'left');
     $this->db->join('tr_product_best_seller c', 'c.prod_id = a.prod_id', 'left');
-    $this->db->join('tm_product d', 'd.id = a.prod_id', 'left');
+    $this->db->join('tr_product_image d', 'd.id_prod = a.prod_id', 'left');
     $this->db->where('b.id', $idProduct);
     $query = $this->db->get();
     if ($query->num_rows() != 0) {
