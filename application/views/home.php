4b63fcf92d383a609d90ce779ba7a664f4854c03
Adrian Faisal <adrianfaisal@aol.com>
2019-07-07 22:42:52 +0700

    change online promotions section

--- application/views/home.php
+++ application/views/home.php
@@ -107,7 +107,7 @@
             </div>
         </div>
         <div class="row">
-<!--            # 1 Best seller-->
+            <!--            # 1 Best seller-->
             <div class="col-12 col-md-4 pt-30">
                 <div class="shop-item bg-white">
                     <div class="thumbnail">
@@ -117,7 +117,9 @@
                         </div>
 
                         <a class="shop-item-image" href="#">
-                            <img class="img-fluid" style="position: relative; z-index: auto" src="<?= site_url('asset/upload/serta-mattress-grand_althea.jpg');?>" alt="product name" />
+                            <img class="img-fluid" style="position: relative; z-index: auto"
+                                 src="<?= site_url('asset/upload/serta-mattress-grand_althea.jpg'); ?>"
+                                 alt="product name"/>
                         </a>
                         <!-- /product image(s) -->
 
@@ -132,10 +134,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <small class="text-muted">Was Rp <strike><?= number_format(225000000, 2, ",", ".");?></strike></small>
+                            <small class="text-muted">Was Rp <strike><?= number_format(225000000, 2, ",",
+                                        "."); ?></strike></small>
                             <!-- price -->
                             <div class="shop-item-price">
-                                Rp <?= number_format(125000000, 2, ",", ".");?>
+                                Rp <?= number_format(125000000, 2, ",", "."); ?>
                             </div>
                             <!-- /price -->
                         </div>
@@ -143,7 +146,7 @@
                 </div>
             </div>
 
-<!--            #2 Bed Linen-->
+            <!--            #2 Bed Linen-->
             <div class="col-12 col-md-4 pt-30">
                 <div class="shop-item bg-white">
                     <div class="thumbnail">
@@ -152,7 +155,9 @@
                             <p class="text-center" style="letter-spacing: 3px;"><b>BED LINEN</b></p>
                         </div>
                         <a class="shop-item-image" href="#">
-                            <img class="img-fluid" style="position: relative; z-index: auto" src="<?= site_url('asset/upload/serta-mattress-grand_althea.jpg');?>" alt="product name" />
+                            <img class="img-fluid" style="position: relative; z-index: auto"
+                                 src="<?= site_url('asset/upload/serta-mattress-grand_althea.jpg'); ?>"
+                                 alt="product name"/>
                         </a>
                         <!-- /product image(s) -->
 
@@ -167,10 +172,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <small class="text-muted">Was Rp <strike><?= number_format(225000000, 2, ",", ".");?></strike></small>
+                            <small class="text-muted">Was Rp <strike><?= number_format(225000000, 2, ",",
+                                        "."); ?></strike></small>
                             <!-- price -->
                             <div class="shop-item-price">
-                                Rp <?= number_format(125000000, 2, ",", ".");?>
+                                Rp <?= number_format(125000000, 2, ",", "."); ?>
                             </div>
                             <!-- /price -->
                         </div>
@@ -178,7 +184,7 @@
                 </div>
             </div>
 
-<!--            #3 Bedding Accessories-->
+            <!--            #3 Bedding Accessories-->
             <div class="col-12 col-md-4 pt-30">
                 <div class="shop-item bg-white">
                     <div class="thumbnail">
@@ -187,7 +193,9 @@
                             <p class="text-center" style="letter-spacing: 3px;"><b>BEDDING ACCESSORIES</b></p>
                         </div>
                         <a class="shop-item-image" href="#">
-                            <img class="img-fluid" style="position: relative; z-index: auto" src="<?= site_url('asset/upload/serta-mattress-grand_althea.jpg');?>" alt="product name" />
+                            <img class="img-fluid" style="position: relative; z-index: auto"
+                                 src="<?= site_url('asset/upload/serta-mattress-grand_althea.jpg'); ?>"
+                                 alt="product name"/>
                         </a>
                         <!-- /product image(s) -->
 
@@ -202,10 +210,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <small class="text-muted">Was Rp <strike><?= number_format(225000000, 2, ",", ".");?></strike></small>
+                            <small class="text-muted">Was Rp <strike><?= number_format(225000000, 2, ",",
+                                        "."); ?></strike></small>
                             <!-- price -->
                             <div class="shop-item-price">
-                                Rp <?= number_format(125000000, 2, ",", ".");?>
+                                Rp <?= number_format(125000000, 2, ",", "."); ?>
                             </div>
                             <!-- /price -->
                         </div>
@@ -213,60 +222,68 @@
                 </div>
             </div>
 
-<!--            <div class="col-12 col-md-4 pt-30">-->
-<!--                <a class="mt-10" href="--><?//= base_url('home/bestSeller'); ?><!--">-->
-<!--                    <img class="promotion-image-1"-->
-<!--                         src="--><?//= base_url('asset/upload/best-seller-cover/'.$best_seller['slide']); ?><!--"-->
-<!--                         alt="agm best seller"/>-->
-<!--                    <div class="absolute pl-20 pb-20 bottom-center text-left">-->
-<!--                        <h2 class="text-grey">BEST<br>SELLER</h2>-->
-<!--                        <a class="text-grey mt-10" href="--><?//= base_url('home/bestSeller'); ?><!--">View Detail<i-->
-<!--                                    class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="col-12 col-md-8">-->
-<!--                <div class="row">-->
-<!--                    <div class="col-md-12 pt-30">-->
-<!--                        <a href="--><?//= base_url('home/specialPackage'); ?><!--">-->
-<!--                            <div class="relative">-->
-<!--                                <img class="promotion-image-2"-->
-<!--                                     src="--><?//= base_url('asset/upload/special-package/cover/'.$spPackage['slide']); ?><!--"-->
-<!--                                     alt="agm gallery"/>-->
-<!--                                <div class="absolute pl-20 pb-20 bottom-center text-left">-->
-<!--                                    <h2 class="text-grey">SPECIAL<br>PACKAGES</h2>-->
-<!--                                    <a class="text-grey" href="--><?//= base_url('home/specialPackage'); ?><!--">View Detail<i-->
-<!--                                                class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
-<!--                                </div>-->
-<!--                        </a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="col-md-6 pt-30">-->
-<!--                    <a href="--><?//= base_url('home/bed_linen'); ?><!--">-->
-<!--                        <img class="promotion-image-3"-->
-<!--                             src="--><?//= base_url('asset/upload/bed-linen-cover/'.$bedLinen['slide']); ?><!--"-->
-<!--                             alt="agm lastest collectin"/>-->
-<!--                        <div class="absolute pl-20 pb-20 bottom-center text-left">-->
-<!--                            <h2 class="text-grey">BED<br>LINEN</h2>-->
-<!--                            <a class="text-grey" href="--><?//= base_url('home/bed_linen'); ?><!--">View Detail<i-->
-<!--                                        class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
-<!--                        </div>-->
-<!--                    </a>-->
-<!--                </div>-->
-<!--                <div class="col-md-6 pt-30">-->
-<!--                    <a href="--><?//= base_url('home/bedding_acc'); ?><!--">-->
-<!--                        <img class="promotion-image-3"-->
-<!--                             src="--><?//= base_url('asset/upload/bedding-acc-cover/'.$beddingAcc['slide']); ?><!--"-->
-<!--                             alt="agm lastest collectin"/>-->
-<!--                        <div class="absolute pl-20 pb-20 bottom-center text-left">-->
-<!--                            <h2 class="text-grey">BEDDING<br>ACCESSORIES</h2>-->
-<!--                            <a class="text-grey" href="--><?//= base_url('home/bedding_acc'); ?><!--">View Detail<i-->
-<!--                                        class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
-<!--                        </div>-->
-<!--                    </a>-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div class="col-12 col-md-4 pt-30">-->
+            <!--                <a class="mt-10" href="--><? //= base_url('home/bestSeller'); ?><!--">-->
+            <!--                    <img class="promotion-image-1"-->
+            <!--                         src="-->
+            <? //= base_url('asset/upload/best-seller-cover/'.$best_seller['slide']); ?><!--"-->
+            <!--                         alt="agm best seller"/>-->
+            <!--                    <div class="absolute pl-20 pb-20 bottom-center text-left">-->
+            <!--                        <h2 class="text-grey">BEST<br>SELLER</h2>-->
+            <!--                        <a class="text-grey mt-10" href="-->
+            <? //= base_url('home/bestSeller'); ?><!--">View Detail<i-->
+            <!--                                    class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
+            <!--                    </div>-->
+            <!--                </a>-->
+            <!--            </div>-->
+            <!---->
+            <!--            <div class="col-12 col-md-8">-->
+            <!--                <div class="row">-->
+            <!--                    <div class="col-md-12 pt-30">-->
+            <!--                        <a href="--><? //= base_url('home/specialPackage'); ?><!--">-->
+            <!--                            <div class="relative">-->
+            <!--                                <img class="promotion-image-2"-->
+            <!--                                     src="-->
+            <? //= base_url('asset/upload/special-package/cover/'.$spPackage['slide']); ?><!--"-->
+            <!--                                     alt="agm gallery"/>-->
+            <!--                                <div class="absolute pl-20 pb-20 bottom-center text-left">-->
+            <!--                                    <h2 class="text-grey">SPECIAL<br>PACKAGES</h2>-->
+            <!--                                    <a class="text-grey" href="-->
+            <? //= base_url('home/specialPackage'); ?><!--">View Detail<i-->
+            <!--                                                class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
+            <!--                                </div>-->
+            <!--                        </a>-->
+            <!--                    </div>-->
+            <!--                </div>-->
+            <!--                <div class="col-md-6 pt-30">-->
+            <!--                    <a href="--><? //= base_url('home/bed_linen'); ?><!--">-->
+            <!--                        <img class="promotion-image-3"-->
+            <!--                             src="-->
+            <? //= base_url('asset/upload/bed-linen-cover/'.$bedLinen['slide']); ?><!--"-->
+            <!--                             alt="agm lastest collectin"/>-->
+            <!--                        <div class="absolute pl-20 pb-20 bottom-center text-left">-->
+            <!--                            <h2 class="text-grey">BED<br>LINEN</h2>-->
+            <!--                            <a class="text-grey" href="-->
+            <? //= base_url('home/bed_linen'); ?><!--">View Detail<i-->
+            <!--                                        class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
+            <!--                        </div>-->
+            <!--                    </a>-->
+            <!--                </div>-->
+            <!--                <div class="col-md-6 pt-30">-->
+            <!--                    <a href="--><? //= base_url('home/bedding_acc'); ?><!--">-->
+            <!--                        <img class="promotion-image-3"-->
+            <!--                             src="-->
+            <? //= base_url('asset/upload/bedding-acc-cover/'.$beddingAcc['slide']); ?><!--"-->
+            <!--                             alt="agm lastest collectin"/>-->
+            <!--                        <div class="absolute pl-20 pb-20 bottom-center text-left">-->
+            <!--                            <h2 class="text-grey">BEDDING<br>ACCESSORIES</h2>-->
+            <!--                            <a class="text-grey" href="-->
+            <? //= base_url('home/bedding_acc'); ?><!--">View Detail<i-->
+            <!--                                        class="fa fa-long-arrow-right pl-5" aria-hidden="true"></i></a>-->
+            <!--                        </div>-->
+            <!--                    </a>-->
+            <!--                </div>-->
+            <!--            </div>-->
         </div>
     </div>
     </div>
@@ -275,75 +292,20 @@
 
 <hr>
 
-<!-- AGMPEDIA -->
+<!-- Online Promotions / Special Package -->
 <section id="agmpedia" class="section-xs container">
-    <a href="list-article.html">
-        <h3 class="text-center">AGMPEDIA</h3>
-        <h5 class="text-center">lastest news of week</h5>
-    </a>
-    <?php
-    $i = 1;
-    foreach ($pedias as $pedia):
-        if ($i % 2 == 1) { ?>
-            <div class="row">
-                <div class="col-12 col-md-5 pb-5">
-                    <div class="buttons-autohide controlls-over mb-30"
-                         data-plugin-options='{"singleItem": true, "navigation": false, "autoPlay": 3000, "pagination": false, "transitionStyle":"fade"}'>
-                        <div>
-                            <img class="pedia-img-1" src="<?= base_url('asset/content-images/slider-1.jpg'); ?>"
-                                 style="width:100%;" alt="agmpedia title">
-                            <div class="absolute pt-30">
-                                <h4 class="text-white text-center mb-20"><?= $pedia['title']; ?></h4>
-                                <p class="fs-13 text-white text-justify pedia-text hidden-xs-down hidden-md-down just-hidden">
-                                    <?= $pedia['sub_content']; ?>
-                                </p>
-                                <p class="text-center to-center fs-13 pt-10">
-                                    <a class="text-white block"
-                                       href="<?= site_url('home/fullArticle/'.$pedia['id']); ?>">read more<i
-                                                class="fa fa-chevron-right pl-5"
-                                                aria-hidden="true"></i></a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-7 pb-5">
-                    <div class="buttons-autohide controlls-over mb-30"
-                         data-plugin-options='{"singleItem": true, "navigation": false, "autoPlay": 3000, "pagination": false, "transitionStyle":"fade"}'>
-                        <img class="pedia-img-1" src="<?= base_url('asset/upload/pedia/'.$pedia['thumbnail']); ?>"
-                             alt="agmpedia">
-                    </div>
+    <div class="row">
+        <div class="col-12 col-md-12">
+            <img class="pedia-img-1" src="<?= site_url('asset/upload/UpperBannerAGM.jpg'); ?>"
+                 alt="agmpedia">
+            <div class="absolute justify-content-around pl-20 pb-20 text-left bg-gradient-red">
+                <div class="ml-50 mt-150">
+                    <h1 class="text-white mb-0">ONLINE<br>PROMOTION</h1>
+                    <a class="btn btn-outline-white mt-10" href="<?= base_url('home/bestSeller'); ?>">Discover more</a>
                 </div>
             </div>
-        <?php } else { ?>
-            <div class="row">
-                <div class="col-12 col-md-7 order-2 order-md-1">
-                    <img class="pedia-img-1" src="<?= base_url('asset/upload/pedia/'.$pedia['thumbnail']); ?>"
-                         alt="agmpedia">
-                </div>
-                <div class="col-12 col-md-5 pb-30 order-1 order-md-2">
-                    <div class="buttons-autohide controlls-over mb-30"
-                         data-plugin-options='{"singleItem": true, "navigation": false, "autoPlay": 3000, "pagination": false, "transitionStyle":"fade"}'>
-                        <div>
-                            <img class="pedia-img-1" src="<?= base_url('asset/content-images/slider-1.jpg'); ?>"
-                                 style="width:100%;" alt="agmpedia title">
-                            <div class="absolute pt-20">
-                                <h4 class="text-white text-center mb-20"><?= $pedia['title']; ?></h4>
-                                <p class="fs-13 text-white text-justify pedia-text hidden-xs-down hidden-md-down just-hidden">
-                                    <?= $pedia['sub_content']; ?>
-                                </p>
-                                <p class="text-center to-center fs-13 pt-10">
-                                    <a class="text-white" href="<?= site_url('home/fullArticle/'.$pedia['id']); ?>">read
-                                        more<i class="fa fa-chevron-right pl-5"
-                                               aria-hidden="true"></i></a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php }
-        $i++;
-    endforeach; ?>
+        </div>
+    </div>
 </section>
 <!-- /AGMPEDIA -->
 
