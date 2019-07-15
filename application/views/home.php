308c41e98ee68016a326ac9a17011e19609cd00e
Adrian Faisal <adrianfaisal@aol.com>
2019-07-15 19:54:37 +0700

    move maps to another page

--- application/views/home.php
+++ application/views/home.php
@@ -461,11 +461,11 @@
 </section>
 <!-- /Product Hightlight -->
 
-<!-- LOCATION -->
-<!--<section id="location" class="mb-30">-->
-<!--    <div class="container-fluid">-->
-<!--        <h3 class="text-center mb-30">OUR LOCATION</h3>-->
-<!--        <div id="store-location"></div>-->
-<!--    </div>-->
-<!--</section>-->
+ LOCATION
+<section id="location" class="mb-30">
+    <div class="container-fluid">
+        <h3 class="text-center mb-30">OUR LOCATION</h3>
+        <div id="store-location"></div>
+    </div>
+</section>
 <!-- /LOCATION -->
